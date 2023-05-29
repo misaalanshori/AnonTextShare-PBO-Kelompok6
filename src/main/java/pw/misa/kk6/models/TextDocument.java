243698f279fd23bdb7d0ce66ab300327969c1ea4
M Isa Al Anshori <isabungsu@gmail.com>
2023-05-29 16:42:27 +0700

    Kerangka Bayangan

---
+++ src/main/java/pw/misa/kk6/models/TextDocument.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pw.misa.kk6.models;
+
+/**
+ *
+ * @author Isabu
+ */
+public class TextDocument {
+    private class Comment {
+        
+    } 
+}
