437b5f8ba0ce628d38f770fbd73e9af31ecaa649
M Isa Al Anshori <isabungsu@gmail.com>
2023-06-02 17:38:19 +0700

    buat class koneksi database

--- src/main/java/pw/misa/kk6/db/DBConnection.java
+++ src/main/java/pw/misa/kk6/db/DBConnection.java
@@ -8,6 +8,39 @@ package pw.misa.kk6.db;
  *
  * @author Isabu
  */
+import java.sql.CallableStatement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DBConnection {
+    private static Connection connection;
+
+    private DBConnection() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            if (connection == null) {
+                try {
+                    Class.forName("oracle.jdbc.OracleDriver");
+                    String jdbcUrl = "jdbc:oracle:thin:@db.kk6.misa.pw:1251:xe";
+                    String username = "public_user";
+                    String password = "helloanonymous";
+                    connection = DriverManager.getConnection(jdbcUrl, username, password);
+                    connection.setAutoCommit(false);
+                } catch (ClassNotFoundException | SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        return connection;
+    }
     
+
+    }
 }
