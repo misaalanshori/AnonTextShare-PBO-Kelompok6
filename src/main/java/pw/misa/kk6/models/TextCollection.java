9ea74c936296ca35c2c46fb67f04cd07216edc93
reyhanhaidar <azanireyhan@student.telkomuniversity.ac.id>
2023-06-04 16:26:28 +0700

    model1

--- src/main/java/pw/misa/kk6/models/TextCollection.java
+++ src/main/java/pw/misa/kk6/models/TextCollection.java
@@ -4,10 +4,114 @@
  */
 package pw.misa.kk6.models;
 
+import java.util.ArrayList;
+import java.util.List;
+import pw.misa.kk6.AnonymousText.Document;
+import pw.misa.kk6.Database.DatabaseConnection;
+
 /**
  *
  * @author Isabu
  */
 public class TextCollection {
-    
+        private DatabaseConnection Access;
+
+	private String ID;
+
+	private String Pass;
+
+	public String Title;
+
+	public List<Document> Contents;
+
+	public TextCollection(DatabaseConnection database) {
+            this.Access = database;
+            this.ID = null;
+            this.Pass = null;
+            this.Title = "";
+            this.Contents = null;
+	}
+
+	public TextCollection(DatabaseConnection database, String CollectionID) {
+            this.Access = database;
+            this.ID = CollectionID;
+            this.Pass = null;
+            reload();
+	}
+
+	public TextCollection(DatabaseConnection database, String CollectionID, String CollectionPass) {
+            this.Access = database;
+            this.ID = CollectionID;
+            this.Pass = CollectionPass;
+            reload();
+	}
+
+	public void reload() {
+            if(this.ID != null){
+                if(Access.checkCollection(ID)){
+                    this.Title = Access.getCollectionTitle(ID);
+                    List<String> ListBaru = Access.getCollectionContents(ID);
+                    List<Document> DocumentBaru = new ArrayList();
+                    for(int i = 0 ; i < ListBaru.size(); i++){
+                        if (this.Access.checkDocument(ListBaru.get(i))) {
+                            DocumentBaru.add(new Document(Access, ListBaru.get(i)));
+                        }
+                        
+                    }
+                    Contents = DocumentBaru;
+                }
+            }
+	}
+
+	public void save() {
+            List<String> ListBaru = new ArrayList();
+            for(int i = 0 ; i < this.Contents.size(); i++){
+                ListBaru.add(this.Contents.get(i).getDocumentID());
+            }
+            
+            if(Pass != null && ID == null){
+                this.ID = Access.createCollection(Title, ListBaru, this.Pass);
+            }else if(ID == null){
+                this.ID = Access.createCollection(this.Title, ListBaru); 
+            }else if(!isReadOnly()){
+                Access.updateCollectionTitle(ID, Pass, Title);
+                Access.updateCollectionContents(ID, Pass, ListBaru);
+            }else{
+                System.out.println("Collection Read Only");
+            }
+	}
+
+	public boolean isReadOnly() {
+            if(this.ID == null){
+                return false;
+            }else if(Pass != null){   
+                return !Access.checkCollection(ID, Pass);
+            }else{
+                return true;
+            }		
+	}
+
+        public int getViewCount() {
+            return this.Access.getCollectionViews(ID);
+        }
+        
+        public boolean delete() {
+            if (!isReadOnly()) {
+                this.Access.deleteDocument(ID, Pass);
+                this.ID = null;
+                this.Pass = null;
+                this.Title = "";
+                this.Contents = null;
+                return true;
+            }
+            return false;
+        }
+        
+	public void loadPassword(String CollectionPass) {
+             this.Pass = CollectionPass; 
+	}
+
+	public String getCollectionID() {
+            return ID;
+	}
 }
--- src/main/java/pw/misa/kk6/models/TextDocument.java
+++ src/main/java/pw/misa/kk6/models/TextDocument.java
@@ -4,12 +4,154 @@
  */
 package pw.misa.kk6.models;
 
+import java.util.List;
+import pw.misa.kk6.Database.DatabaseConnection;
+
 /**
  *
  * @author Isabu
  */
 public class TextDocument {
     private class Comment {
-        
+        private String name;
+        private String text;
+
+    public Comment(String name, String text) {
+        this.name = name;
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getText() {
+        return text;
+    }
+    
     } 
+    private DatabaseConnection DatabaseAccess;
+
+	private String ID;
+
+	private String Pass;
+
+	public String Title;
+
+	public String Text;
+        
+        private int visibility;
+        
+        private List<pw.misa.kk6.AnonymousText.Comment> DocumentComments;
+
+	public TextDocument(DatabaseConnection database) {
+            this.DatabaseAccess = database;
+            this.ID = null;
+            this.Pass = null;
+            this.DocumentComments = null;
+            this.Title = "";
+            this.Text = "";
+            this.visibility = 1;
+	}
+
+	public TextDocument(DatabaseConnection database, String DocumentID) {
+            if(database.checkDocument(DocumentID)){
+                this.DatabaseAccess = database;
+                this.ID = DocumentID;
+                this.Pass = null;
+                reload();
+            }else {
+                System.out.println("Document not found.");
+            }
+	}
+
+	public TextDocument(DatabaseConnection database, String DocumentID, String DocumentPass) {
+            if(database.checkDocument(DocumentID, DocumentPass)){
+                this.DatabaseAccess = database;
+                this.ID = DocumentID;
+                this.Pass = DocumentPass;
+                reload();
+            }else {
+                System.out.println("Document not found.");
+            }
+	}
+
+	public void reload() {
+            if (this.ID != null) {
+                this.Title = this.DatabaseAccess.getDocumentTitle(this.ID);
+                this.Text = this.DatabaseAccess.getDocumentText(this.ID);
+                this.DocumentComments = this.DatabaseAccess.getDocumentComments(ID);
+                this.visibility = this.DatabaseAccess.getDocumentVisibility(ID);
+            }
+	}
+
+	public void save() {
+            if (this.ID == null) {
+                if (this.Pass != null && !this.Pass.isEmpty()) {
+                    this.ID = this.DatabaseAccess.createDocument(this.Title, this.Text, this.Pass, this.visibility);
+                } else {
+                    this.ID = this.DatabaseAccess.createDocument(this.Title, this.Text, this.visibility);
+                }   
+            } else if (!isReadOnly()) {
+                this.DatabaseAccess.updateDocumentTitle(this.ID, this.Pass, this.Title);
+                this.DatabaseAccess.updateDocumentText(this.ID, this.Pass, this.Text);
+                this.DatabaseAccess.updateDocumentVisibility(this.ID, this.Pass, this.visibility);
+            }
+	}
+
+	public boolean isReadOnly() {
+            if (this.ID == null) {
+                return false;
+            } else {
+                if (this.Pass == null || this.Pass.isEmpty()) {
+                    return true;
+                } else {
+                    return !this.DatabaseAccess.checkDocument(this.ID, this.Pass);
+                }
+            }
+	}
+        
+        public void addComment(String name, String text) {
+            if (this.ID != null) {
+                this.DatabaseAccess.addDocumentComment(ID, name, text);
+                reload();
+            }
+        }
+        
+        public List<pw.misa.kk6.AnonymousText.Comment> getComments() {
+            return DocumentComments;
+        }
+        
+        public int getViewCount() {
+            return this.DatabaseAccess.getDocumentViews(ID);
+        }
+        
+        public boolean delete() {
+            if (!isReadOnly()) {
+                this.DatabaseAccess.deleteDocument(ID, Pass);
+                this.ID = null;
+                this.Pass = null;
+                this.Title = "";
+                this.Text = "";
+                this.visibility = 0;
+                return true;
+            }
+            return false;
+        }
+
+	public void loadPassword(String DocumentPass) {
+            this.Pass = DocumentPass;
+	}
+
+        public void setVisibility(int visibility) {
+            this.visibility = visibility;
+        }
+
+        public int getVisibility() {
+            return this.visibility;
+        }
+
+	public String getDocumentID() {
+            return this.ID;
+	}
 }
