11d84321cbb84fa94f6f144871bf01784b0b1b36
M Isa Al Anshori <isabungsu@gmail.com>
2023-06-05 19:01:33 +0700

    tambah prosedur dan function akses dokumen

--- src/main/java/pw/misa/kk6/db/anondb.sql
+++ src/main/java/pw/misa/kk6/db/anondb.sql
@@ -1,6 +1,14 @@
 -- Dropping the public user
 DROP USER public_user CASCADE;
 
+-- Dropping the procedures/functions
+DROP PROCEDURE create_document;
+DROP PROCEDURE create_document_nopass;
+DROP FUNCTION check_document_password;
+DROP PROCEDURE update_document;
+DROP PROCEDURE delete_document;
+DROP PROCEDURE increment_view_count;
+
 -- Dropping the views
 DROP VIEW latest_documents_view;
 DROP VIEW documents_view;
@@ -75,3 +83,140 @@ WHERE ROWNUM <= 10;
 CREATE VIEW documents_view AS
 SELECT id, view_count, title, text, visibility, time_created
 FROM document;
+
+CREATE OR REPLACE PROCEDURE create_document
+(
+    id OUT document.id%TYPE,
+    password IN VARCHAR2,
+    title IN document.title%TYPE,
+    text IN document.text%TYPE,
+    visibility IN document.visibility%TYPE
+) IS
+    hashed_password VARCHAR2(64);
+BEGIN
+    id := SYS_GUID();
+    hashed_password := LOWER(RAWTOHEX(DBMS_CRYPTO.HASH(UTL_I18N.STRING_TO_RAW(password, 'AL32UTF8'), DBMS_CRYPTO.HASH_SH1)));
+    -- Insert data into the document table
+    INSERT INTO document (id, password_hash, title, text, visibility, time_created)
+    VALUES (id, hashed_password, title, text, visibility, CURRENT_TIMESTAMP);
+    -- Commit the transaction
+    COMMIT;
+END create_document;
+/
+
+CREATE OR REPLACE PROCEDURE create_document_nopass
+(
+    id OUT document.id%TYPE,
+    title IN document.title%TYPE,
+    text IN document.text%TYPE,
+    visibility IN document.visibility%TYPE
+) IS
+BEGIN
+    id := SYS_GUID();
+    -- Insert data into the document table
+    INSERT INTO document (id, password_hash, title, text, visibility, time_created)
+    VALUES (id, null, title, text, visibility, CURRENT_TIMESTAMP);
+    -- Commit the transaction
+    COMMIT;
+END create_document_nopass;
+/
+
+CREATE OR REPLACE FUNCTION check_document_password
+(
+    p_id IN VARCHAR2,
+    p_password IN VARCHAR2
+) RETURN BOOLEAN IS
+    v_password_hash VARCHAR2(64);
+    v_hashed_password VARCHAR2(64);
+BEGIN
+    -- Retrieve the password hash for the given ID
+    SELECT password_hash INTO v_password_hash
+    FROM document
+    WHERE id = p_id;
+
+    -- Check if the ID is found and the password hash is not NULL
+    IF v_password_hash IS NULL THEN
+        RETURN FALSE; -- Password hash is NULL, indicating invalid ID
+    END IF;
+
+    -- Hash the provided password using SHA-1
+    v_hashed_password := LOWER(RAWTOHEX(DBMS_CRYPTO.HASH(UTL_I18N.STRING_TO_RAW(p_password, 'AL32UTF8'), DBMS_CRYPTO.HASH_SH1)));
+  
+    -- Compare the hashed password with the stored password hash
+    IF v_password_hash = v_hashed_password THEN
+        RETURN TRUE; -- Password is verified
+    ELSE
+        RETURN FALSE; -- Password is not verified
+    END IF;
+END check_document_password;
+/
+
+CREATE OR REPLACE PROCEDURE update_document
+(
+    id IN document.id%TYPE,
+    password IN VARCHAR2,
+    title IN document.title%TYPE,
+    text IN document.text%TYPE,
+    visibility IN document.visibility%TYPE
+) IS
+    v_password_hash VARCHAR2(64);
+BEGIN
+    -- Check the password
+    IF check_document_password(id, password) THEN
+        -- Update the document table
+        UPDATE document SET
+            title = title,
+            text = text,
+            visibility = visibility
+        WHERE id = id;
+    ELSE
+        RAISE_APPLICATION_ERROR(-21001, 'Incorrect Document password');
+    END IF;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        -- Document with given ID not found
+        RAISE_APPLICATION_ERROR(-21002, 'Document not found');
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-29999, 'An Exception Occured');
+END update_document;
+/
+
+CREATE OR REPLACE PROCEDURE delete_document
+(
+    p_id IN document.id%TYPE,
+    password IN VARCHAR2
+) IS
+    valid_password BOOLEAN;
+BEGIN
+    -- Check if the provided password matches the password_hash for the given id
+    valid_password := check_document_password(p_id, password);
+
+    IF valid_password THEN
+        -- Delete the document if the password is valid
+        DELETE FROM document WHERE id = p_id;
+    ELSE
+        RAISE_APPLICATION_ERROR(-21001, 'Incorrect Document password');
+    END IF;
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-29999, 'An Exception Occured');
+END delete_document;
+/
+
+CREATE OR REPLACE PROCEDURE increment_view_count
+(
+    document_id IN document.id%TYPE
+) IS
+BEGIN
+    -- Increment view_count by 1
+    UPDATE document SET
+        view_count = view_count + 1
+    WHERE id = document_id;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        -- Document with given ID not found
+        RAISE_APPLICATION_ERROR(-21002, 'Document not found');
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-29999, 'An Exception Occured');
+END increment_view_count;
+/
