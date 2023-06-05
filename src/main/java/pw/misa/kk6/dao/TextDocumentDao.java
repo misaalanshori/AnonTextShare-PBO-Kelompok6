6e315f720f11a0387e2b3d2cba9bbec866a8d384
M Isa Al Anshori <isabungsu@gmail.com>
2023-06-05 22:18:27 +0700

    implemetasi awal DAO TextDocument

--- src/main/java/pw/misa/kk6/dao/TextDocumentDao.java
+++ src/main/java/pw/misa/kk6/dao/TextDocumentDao.java
@@ -4,7 +4,15 @@
  */
 package pw.misa.kk6.dao;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.ArrayList;
 import java.util.List;
+import pw.misa.kk6.db.DBConnection;
 import pw.misa.kk6.models.TextDocument;
 
 /**
@@ -12,28 +20,139 @@ import pw.misa.kk6.models.TextDocument;
  * @author Isabu
  */
 public class TextDocumentDao {
+    private Connection conn;
+
+    public TextDocumentDao() {
+        conn = DBConnection.getConnection();
+    }
+    
+    
     public String insert(TextDocument document) {
+        try {
+            CallableStatement cStmt;
+            if (document.getPass() != null && !document.getPass().isBlank()) {
+                String sql = "{CALL sys.create_document(?, ?, ?, ?, ?)}";
+                cStmt = conn.prepareCall(sql);
+                cStmt.registerOutParameter(1, Types.VARCHAR);
+                cStmt.setString(2, document.getPass());
+                cStmt.setString(3, document.getTitle());
+                cStmt.setString(4, document.getText());
+                cStmt.setInt(5, document.getVisibility());
+                System.out.println("INFO: Dokumen inserted dengan password");
+            } else {
+                String sql = "{CALL sys.create_document_nopass(?, ?, ?, ?, )}";
+                cStmt = conn.prepareCall(sql);
+                cStmt.registerOutParameter(1, Types.VARCHAR);
+                cStmt.setString(2, document.getTitle());
+                cStmt.setString(3, document.getText());
+                cStmt.setInt(4, document.getVisibility());
+                System.out.println("INFO: Dokumen inserted tanpa password");
+            }
+            return cStmt.getString(1);
+            
+        } catch (SQLException e) {
+            System.out.println("ERROR: Terjadi Kesalahan SQL: " + e.getMessage());
+        }
         return null;
     }
     
     public void update(TextDocument document) {
-        
+        try {
+            String sql = "{CALL sys.update_document(?, ?, ?, ?, ?)}";
+            CallableStatement cStmt = conn.prepareCall(sql);
+            cStmt.setString(1, document.getID());
+            cStmt.setString(2, document.getPass());
+            cStmt.setString(3, document.getTitle());
+            cStmt.setString(4, document.getText());
+            cStmt.setInt(5, document.getVisibility());
+            System.out.println("INFO: Dokumen updated");
+        } catch (SQLException e) {
+            System.out.println("ERROR: Terjadi Kesalahan SQL: " + e.getMessage());
+        }
     }
     
     public void delete(String id, String pass) {
-        
+         try {
+            String sql = "{CALL sys.delete_document(?, ?)}";
+            CallableStatement cStmt = conn.prepareCall(sql);
+            cStmt.setString(1, id);
+            cStmt.setString(2, pass);
+            System.out.println("INFO: Dokumen deleted");
+        } catch (SQLException e) {
+            System.out.println("ERROR: Terjadi Kesalahan SQL: " + e.getMessage());
+        }
     }
     
     // Method untuk menambahkan komentar pada dokumen
     public void insertComment(String id, TextDocument.Comment comment) {
-        
+        try {
+            String sql = "{CALL sys.insert_document_comment(?, ?, ?)}";
+            CallableStatement cStmt = conn.prepareCall(sql);
+            cStmt.setString(1, id);
+            cStmt.setString(2, comment.getName());
+            cStmt.setString(3, comment.getText());
+            System.out.println("INFO: Comment inserted");
+        } catch (SQLException e) {
+            System.out.println("ERROR: Terjadi Kesalahan SQL: " + e.getMessage());
+        }
     }
-    
+
     public TextDocument select(String id) {
-        return null;
+        
+        TextDocument doc = null;
+        ArrayList<TextDocument.Comment> commentList = new ArrayList<>();
+        PreparedStatement stmt;
+        ResultSet rs;
+        try {
+            stmt = conn.prepareStatement("SELECT * FROM sys.documents_view WHERE id=" + id);
+            rs = stmt.executeQuery();
+            doc = new TextDocument();
+            while (rs.next()) {
+                doc.setID(id);
+                doc.setTitle(rs.getString("title"));
+                doc.setText(rs.getString("text"));
+                doc.setViewCount(rs.getInt("view_count"));
+                doc.setVisibility(rs.getInt("visibility"));
+            }
+            
+            stmt = conn.prepareStatement("SELECT * FROM sys.document_comment WHERE document_id=" + id);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                commentList.add(doc.new Comment(rs.getString("name"), rs.getString("text")));
+            }
+            doc.setComments(commentList);
+
+            String sql = "{CALL sys.increment_view_count(?)}";
+            CallableStatement cStmt = conn.prepareCall(sql);
+            cStmt.setString(1, id);
+            System.out.println("INFO: Document Loaded");
+        } catch (SQLException e) {
+            System.out.println("Terjadi Kesalahan SQL: " + e.getMessage());
+        }
+        return doc;
     }
     
     public List<TextDocument> selectLatest(int count) {
-        return null;
+        TextDocument doc = null;
+        ArrayList<TextDocument> docList = new ArrayList<>();
+        PreparedStatement stmt;
+        ResultSet rs;
+        try {
+            stmt = conn.prepareStatement("SELECT * FROM (SELECT id, title, view_count FROM sys.documents_view WHERE visibility = 1 ORDER BY time_created DESC) WHERE ROWNUM <= " + count);
+            rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                doc = new TextDocument();
+                doc.setID(rs.getString("id"));
+                doc.setTitle(rs.getString("title"));
+                doc.setViewCount(rs.getInt("view_count"));
+                doc.setText("");
+                doc.setVisibility(1);
+                docList.add(doc);
+            }
+        } catch (SQLException e) {
+            System.out.println("Terjadi Kesalahan SQL: " + e.getMessage());
+        }
+        return docList;
     }
 }
