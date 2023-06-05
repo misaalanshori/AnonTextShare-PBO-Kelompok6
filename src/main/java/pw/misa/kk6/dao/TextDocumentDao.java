4a1f4399d2eec6351ce70ceaab3e4eff3b4299fc
M Isa Al Anshori <isabungsu@gmail.com>
2023-06-05 12:59:48 +0700

    membuat kerangka untuk DAO

--- src/main/java/pw/misa/kk6/dao/TextDocumentDao.java
+++ src/main/java/pw/misa/kk6/dao/TextDocumentDao.java
@@ -4,10 +4,36 @@
  */
 package pw.misa.kk6.dao;
 
+import java.util.List;
+import pw.misa.kk6.models.TextDocument;
+
 /**
  *
  * @author Isabu
  */
 public class TextDocumentDao {
+    public void insert(TextDocument document) {
     
+    }
+    
+    public void update(TextDocument document) {
+        
+    }
+    
+    public void delete(String id, String pass) {
+        
+    }
+    
+    // Method untuk menambahkan komentar pada dokumen
+    public void insertComment(String id, TextDocument.Comment comment) {
+        
+    }
+    
+    public TextDocument select(String id) {
+        return null;
+    }
+    
+    public List<TextDocument> selectLatest(int count) {
+        return null;
+    }
 }
